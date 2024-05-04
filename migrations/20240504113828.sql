fd17e0d6375b8079492f2f0ccb055cdd49c0d1c9
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2024-05-04 11:56:09 +0000

    ci: fix build issue relating to backfilling existing data

--- migrations/20240504113828.sql
+++ migrations/20240504113828.sql
@@ -1,2 +1,10 @@
-ALTER TABLE users
+-- Backfill existing data in the nickname column
+UPDATE users
+SET nickname = 'my_nickname'
+WHERE nickname IS NULL;
+
+-- Alter the nickname column to set it to NOT NULL
+ALTER users
 ALTER COLUMN nickname SET NOT NULL;
+
+
