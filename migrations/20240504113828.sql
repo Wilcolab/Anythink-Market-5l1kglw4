05bc69abe7db07fbf28e04788b5b6b68291ada3f
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2024-05-04 12:20:51 +0000

    fix: fix ci error

--- migrations/20240504113828.sql
+++ migrations/20240504113828.sql
@@ -1,9 +1,6 @@
--- Backfill existing data in the nickname column
 UPDATE users
 SET nickname = 'my_nickname'
 WHERE nickname IS NULL;
-
--- Alter the nickname column to set it to NOT NULL
 ALTER users
 ALTER COLUMN nickname SET NOT NULL;
 
