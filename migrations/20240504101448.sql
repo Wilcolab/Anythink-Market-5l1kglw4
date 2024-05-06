446654cd32f92f2c6eac0fbc0dd0e3e2b4bd7576
WilcoApp <hello@wilco.gg>
2024-05-06 10:19:50 +0000

    Initial commit

--- migrations/20240504101448.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE users
-ADD COLUMN nickname VARCHAR(20) NULL;
--- migrations/20240504113828.sql
+++
@@ -1,7 +0,0 @@
-UPDATE users
-SET nickname = 'my_nickname'
-WHERE nickname IS NULL;
-ALTER TABLE users
-ALTER COLUMN nickname SET NOT NULL;
-
-
